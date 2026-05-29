b4088865288be6c532e5fd5c73523e2b0c201a9d
olegofriendz <abuga@gmail.com>
2026-05-29 13:02:35 +0500

    update docker-compose.yml - /dev/ttyUSB0 or /dev/null

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 xhost +local:root > /dev/null 2>&1
+if [ -e /dev/ttyUSB0 ]; then
+    export GRBL_DEVICE=/dev/ttyUSB0
+    echo "GRBL станок найден ($GRBL_DEVICE). Запуск с поддержкой перемещения."
+else
+    export GRBL_DEVICE=/dev/null
+    echo "GRBL станок НЕ найден. Запуск без режима перемещения."
+fi
 docker compose up
